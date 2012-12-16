fb1771f4a00254f2d37037a2d6a8189463c66b82
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2012-12-16 13:19:25 -0300

    Adding message

--- lang.hs
+++ lang.hs
@@ -2,9 +2,12 @@ data Expr = NumberE Double
           | StringE String
           | SymbolE String 
           | BoolE   Bool
+          | MessageE { selector :: Expr, messageArgs :: [Expr] }
           deriving Show
 
+reduce (MessageE selector messageArgs) = MessageE (reduce selector) (map reduce messageArgs)
 reduce n = n
+
 
 
 
